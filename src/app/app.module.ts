b970d0899bde3ed0ab7821094a9dc1b12cbc3641
absakalli <bugra.sakalli@sersim.com.tr>
2023-03-17 11:24:22 +0300

    d

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { MaterialModule } from './material/material.module';
+
 import { AddTransitionComponent } from './components/add-transition/add-transition.component';
 import { AddStatusComponent } from './components/add-status/add-status.component';
 
