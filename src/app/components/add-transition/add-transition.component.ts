9e13e5fc7d0a541eb15a3b1a188c8c512c4dfcfa
absakalli <bugra.sakalli@sersim.com.tr>
2023-03-23 14:20:17 +0300

    a

--- src/app/components/add-transition/add-transition.component.ts
+++ src/app/components/add-transition/add-transition.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import * as LeaderLine from 'leader-line-new';
+import { Statu } from 'src/model/statu.model';
 
 @Component({
   selector: 'add-transition',
@@ -7,29 +8,30 @@ import * as LeaderLine from 'leader-line-new';
   styleUrls: ['./add-transition.component.scss'],
 })
 export class AddTransitionComponent {
+  connection: any[][];
+  line: LeaderLine;
   start: any;
   end: any;
-  line: any;
-  lines: any[];
+  startindex: any;
+  endindex: any;
 
-  constructor() {}
-
-  ngOnInit() {
-    this.lines = [];
+  constructor() {
+    this.connection = [];
   }
 
   Line() {
-    if (this.start && this.end && this.start != this.end) {
-      this.line = new LeaderLine(this.start, this.end);
-      this.line.path = 'grid';
-      this.line.startPlug = 'disc';
-      this.line.startSocket = 'right';
-      this.line.endPlug = 'arrow2';
-      this.line.endSocket = 'left';
-      this.line.color = 'black';
-      this.line.size = 3;
-      this.lines.push(this.line);
-      console.log(this.lines);
+    this.line = new LeaderLine(this.start, this.end);
+    this.line.startPlug = 'disc';
+    this.line.startSocket = 'right';
+    this.line.endPlug = 'arrow2';
+    this.line.endSocket = 'left';
+    this.line.color = 'black';
+    this.line.size = 3;
+    if (this.startindex == this.endindex) {
+      this.connection[this.startindex].push(this.line);
+    } else {
+      this.connection[this.startindex].push(this.line);
+      this.connection[this.endindex].push(this.line);
     }
   }
 }
